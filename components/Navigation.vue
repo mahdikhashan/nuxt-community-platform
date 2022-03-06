e0aa9c54be140c19ab519778ae1e08e5481188ab
Mahdi Khashan <mahdikhashan1@gmail.com>
2022-03-06 19:50:01 +0330

    revert primary button

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -21,7 +21,7 @@
               class="mr-sm-2"
               placeholder="Search"
             ></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0 primary" type="submit"
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
               >Search</b-button
             >
           </b-nav-form>
