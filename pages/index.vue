7b3781414adc5a7da6913bbca4004ff4d39024ed
Mahdi Khashan <mahdikhashan1@gmail.com>
2022-03-06 21:31:42 +0330

    add mini-discussion component

--- pages/index.vue
+++ pages/index.vue
@@ -17,6 +17,17 @@
         last-number
       ></b-pagination-nav>
     </div>
+    <div v-for="(discussion, index) in discussions" :key="index">
+      <MiniDiscussion
+        :avatar="discussion.avatar"
+        :author="discussion.author"
+        :type="discussion.type"
+        :views="discussion.views"
+        :comments="discussion.comments"
+        :date="discussion.date"
+        :bookmarked="discussion.bookmarked"
+      ></MiniDiscussion>
+    </div>
   </div>
 </template>
 
@@ -38,6 +49,80 @@ export default {
         {
           text: "Library",
           active: true,
+        },
+      ],
+      discussions: [
+        {
+          avatar: "https://us.v-cdn.net/6030147/uploads/userpics/138/nXPSYL5PE8V87.jpg",
+          author: "Mahdi Khashan",
+          type: "Announcement",
+          views: 200,
+          comments: 3,
+          date: "March 2021",
+          bookmarked: false
+        },
+        {
+          avatar: "https://us.v-cdn.net/6030147/uploads/userpics/138/nXPSYL5PE8V87.jpg",
+          author: "Mahdi Khashan",
+          type: "Announcement",
+          views: 200,
+          comments: 3,
+          date: "March 2021",
+          bookmarked: false
+        },
+        {
+          avatar: "https://us.v-cdn.net/6030147/uploads/userpics/138/nXPSYL5PE8V87.jpg",
+          author: "Mahdi Khashan",
+          type: "Announcement",
+          views: 200,
+          comments: 3,
+          date: "March 2021",
+          bookmarked: false
+        },
+        {
+          avatar: "https://us.v-cdn.net/6030147/uploads/userpics/138/nXPSYL5PE8V87.jpg",
+          author: "Mahdi Khashan",
+          type: "Announcement",
+          views: 200,
+          comments: 3,
+          date: "March 2021",
+          bookmarked: false
+        },
+        {
+          avatar: "https://us.v-cdn.net/6030147/uploads/userpics/138/nXPSYL5PE8V87.jpg",
+          author: "Mahdi Khashan",
+          type: "Announcement",
+          views: 200,
+          comments: 3,
+          date: "March 2021",
+          bookmarked: false
+        },
+        {
+          avatar: "https://us.v-cdn.net/6030147/uploads/userpics/138/nXPSYL5PE8V87.jpg",
+          author: "Mahdi Khashan",
+          type: "Announcement",
+          views: 200,
+          comments: 3,
+          date: "March 2021",
+          bookmarked: false
+        },
+        {
+          avatar: "https://us.v-cdn.net/6030147/uploads/userpics/138/nXPSYL5PE8V87.jpg",
+          author: "Mahdi Khashan",
+          type: "Announcement",
+          views: 200,
+          comments: 3,
+          date: "March 2021",
+          bookmarked: false
+        },
+        {
+          avatar: "https://us.v-cdn.net/6030147/uploads/userpics/138/nXPSYL5PE8V87.jpg",
+          author: "Mahdi Khashan",
+          type: "Announcement",
+          views: 200,
+          comments: 3,
+          date: "March 2021",
+          bookmarked: false
         },
       ],
       pages: 100,
