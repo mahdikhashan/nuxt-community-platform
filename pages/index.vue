6acf8601012fd3f4b8620dac44aa12ea0f467436
Mahdi Khashan <mahdikhashan1@gmail.com>
2022-03-06 21:05:42 +0330

    add alert and pagination

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
     <b-breadcrumb :items="items"></b-breadcrumb>
+    <b-alert variant="secondary" show>
+      Are you receiving SNAP/EBT benefits? The Expensify.org/SNAP-VAX campaign
+      is live and accepting new members. Learn more about receiving $50 for
+      submitting a SNAP receipt and $50 for getting your COVID-19 vaccine here
+      and join today!
+    </b-alert>
+    <div class="mt-3">
+      <h6>Goto first and last button number</h6>
+      <b-pagination-nav
+        v-model="currentPage"
+        :number-of-pages="pages"
+        base-url="#"
+        first-number
+        last-number
+      ></b-pagination-nav>
+    </div>
   </div>
 </template>
 
@@ -24,6 +40,8 @@ export default {
           active: true,
         },
       ],
+      pages: 100,
+      currentPage: 5,
     };
   },
 };
