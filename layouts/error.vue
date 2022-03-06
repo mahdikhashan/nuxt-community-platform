b8dcd29ae17ded77d48ec7fb887233dbeb018ba8
Mahdi Khashan <mahdikhashan1@gmail.com>
2022-03-06 18:55:09 +0330

    fix indent

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,14 @@
 <script>
 export default {
-    props: ['error'],
-    layout: 'error'
-}
+  props: ["error"],
+  layout: "error",
+};
 </script>
 
 <template>
-    <div>
-        <h1 v-if="error.statusCode === 404">Page not found</h1>
-        <h1 v-else>An error occurred</h1>
-        <nuxt-link to="/">Home Page</nuxt-link>
-    </div>
+  <div>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Home Page</nuxt-link>
+  </div>
 </template>
