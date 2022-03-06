07b1174bfc163f0e03f0b53d15e563d01777fea9
Mahdi Khashan <mahdikhashan1@gmail.com>
2022-03-06 19:31:35 +0330

    disable precompiled css of bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,11 @@ export default {
     '@nuxtjs/axios'
   ],
 
+  bootstrapVue: {
+    bootstrapCSS: false, // Or `css: false`
+    bootstrapVueCSS: false // Or `bvCSS: false`
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
