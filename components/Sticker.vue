e9cbae93609e39b9d95aac35abe9b659b1fcfa67
Mahdi Khashan <mahdikhashan1@gmail.com>
2022-04-08 03:05:16 +0430

    add sticker component

---
+++ components/Sticker.vue
@@ -0,0 +1,22 @@
+<template>
+    <div
+    class="
+      flex
+      bg-gray-200
+      items-center
+      justify-between
+      p-4
+      rounded-md
+      border-gray-700
+      border-[1px]
+    "
+  >
+    <div class=""><slot></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Sticker",
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,35 +2,71 @@
   <div>
     <Banner />
     <main class="container mx-auto flex justify-between mt-8">
-      <section class="w-[70%] px-6 py-4">
-        <Notification>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        </Notification>
-        <MiniDiscussion v-for="value in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]" :key="value"></MiniDiscussion>
+      <section class="w-[70%] px-6 py-4 flex flex-col gap-2">
+        <Notification>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</Notification>
+        <Sticker>Are you receiving SNAP/EBT benefits? The Expensify.org/SNAP-VAX campaign is live and accepting new members. Learn more about receiving $50 for submitting a SNAP receipt and $50 for getting your COVID-19 vaccine here and join today!</Sticker>
+        <MiniDiscussion
+          v-for="value in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+          :key="value"
+        ></MiniDiscussion>
       </section>
       <aside class="w-[30%] px-6 py-4">
         <NewPost />
         <section class="w-full">
           <h3 class="font-bold text-lg pt-4 pb-2">Quick Links</h3>
           <ul class="w-full text-sm flex flex-col gap-[6px]">
-            <li class="hover:text-blue-600"><nuxt-link to="/">All Categories</nuxt-link></li>
-            <li class="hover:text-blue-600"><nuxt-link to="/">Recent Discussions</nuxt-link></li>
-            <li class="hover:text-blue-600"><nuxt-link to="/">Activity</nuxt-link></li>
-            <li class="w-full flex flex-auto justify-between hover:text-blue-600"><nuxt-link to="/">My Bookmarks</nuxt-link><span>2</span></li>
-            <li class="w-full flex flex-auto justify-between hover:text-blue-600"><nuxt-link to="/">My Discussions</nuxt-link><span>2</span></li>
-            <li class="w-full flex flex-auto justify-between hover:text-blue-600"><nuxt-link to="/">My Drafts</nuxt-link><span>2</span></li>
-            <li class="w-full flex flex-auto justify-between hover:text-blue-600"><nuxt-link to="/">Unanswered</nuxt-link><span>2</span></li>
-            <li class="hover:text-blue-600"><nuxt-link to="/">Best Of</nuxt-link></li>
+            <li class="hover:text-blue-600">
+              <nuxt-link to="/">All Categories</nuxt-link>
+            </li>
+            <li class="hover:text-blue-600">
+              <nuxt-link to="/">Recent Discussions</nuxt-link>
+            </li>
+            <li class="hover:text-blue-600">
+              <nuxt-link to="/">Activity</nuxt-link>
+            </li>
+            <li class="w-full flex flex-auto justify-between hover:text-blue-600">
+              <nuxt-link to="/">My Bookmarks</nuxt-link>
+              <span>2</span>
+            </li>
+            <li class="w-full flex flex-auto justify-between hover:text-blue-600">
+              <nuxt-link to="/">My Discussions</nuxt-link>
+              <span>2</span>
+            </li>
+            <li class="w-full flex flex-auto justify-between hover:text-blue-600">
+              <nuxt-link to="/">My Drafts</nuxt-link>
+              <span>2</span>
+            </li>
+            <li class="w-full flex flex-auto justify-between hover:text-blue-600">
+              <nuxt-link to="/">Unanswered</nuxt-link>
+              <span>2</span>
+            </li>
+            <li class="hover:text-blue-600">
+              <nuxt-link to="/">Best Of</nuxt-link>
+            </li>
           </ul>
         </section>
         <section>
           <h3 class="font-bold text-lg pt-4 pb-2">Categories</h3>
           <ul class="text-sm flex flex-col gap-[6px]">
-            <li class="w-full flex flex-auto justify-between hover:text-blue-600"><nuxt-link to="/">All Categories</nuxt-link></li>
-            <li class="w-full flex flex-auto justify-between hover:text-blue-600"><nuxt-link to="/">Announcements</nuxt-link><span>201</span></li>
-            <li class="w-full flex flex-auto justify-between hover:text-blue-600"><nuxt-link to="/">Help Docs and Webinars</nuxt-link><span>504</span></li>
-            <li class="w-full flex flex-auto justify-between hover:text-blue-600"><nuxt-link to="/">Questions about community</nuxt-link><span>2.1K</span></li>
-            <li class="w-full flex flex-auto justify-between hover:text-blue-600"><nuxt-link to="/">Community.org</nuxt-link><span>151</span></li>
+            <li class="w-full flex flex-auto justify-between hover:text-blue-600">
+              <nuxt-link to="/">All Categories</nuxt-link>
+            </li>
+            <li class="w-full flex flex-auto justify-between hover:text-blue-600">
+              <nuxt-link to="/">Announcements</nuxt-link>
+              <span>201</span>
+            </li>
+            <li class="w-full flex flex-auto justify-between hover:text-blue-600">
+              <nuxt-link to="/">Help Docs and Webinars</nuxt-link>
+              <span>504</span>
+            </li>
+            <li class="w-full flex flex-auto justify-between hover:text-blue-600">
+              <nuxt-link to="/">Questions about community</nuxt-link>
+              <span>2.1K</span>
+            </li>
+            <li class="w-full flex flex-auto justify-between hover:text-blue-600">
+              <nuxt-link to="/">Community.org</nuxt-link>
+              <span>151</span>
+            </li>
           </ul>
         </section>
         <section class="block">
@@ -58,6 +94,7 @@ import Banner from "../components/Banner.vue";
 import NewPost from "../components/NewPost.vue";
 import MiniDiscussion from "../components/MiniDiscussion.vue";
 import Notification from "../components/Notification.vue";
+import Sticker from "../components/Sticker.vue";
 export default {
   name: "IndexPage",
   data() {
@@ -154,6 +191,6 @@ export default {
       currentPage: 5,
     };
   },
-  components: { Banner, NewPost, MiniDiscussion, Notification }
+  components: { Banner, NewPost, MiniDiscussion, Notification, Sticker }
 };
 </script>
