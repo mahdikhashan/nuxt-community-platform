c979eff9e678badb440f8fe75a61a158bf8867a4
mahdikhashan <mahdikhashan@users.noreply.github.com>
2022-04-27 01:04:24 +0000

    deploy: c3c98197ab322a5c4a0bc670cb438eee9e6adbb1

---
+++ _nuxt/3ab42ea.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{282:function(n,e,t){"use strict";t.r(e);var l=t(13),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n    help of "+n._s(n.$route.params.id)+"\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/static/1651021464/discussions/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/discussions", {data:[{}],fetch:{},mutations:[]});
