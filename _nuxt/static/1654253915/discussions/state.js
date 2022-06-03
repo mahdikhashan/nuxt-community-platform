14c61f952d7b54c9d0fe354100104a8e80880edb
mahdikhashan <mahdikhashan@users.noreply.github.com>
2022-06-03 10:59:04 +0000

    deploy: 20ae11bcb122ad99537404ac0064299627e9b03d

--- _nuxt/static/1654253915/discussions/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654253915",layout:"default",error:a,state:{notification:{},auth:{user:a,loggedIn:false,strategy:""}},serverRendered:true,routePath:"\u002Fdiscussions",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
