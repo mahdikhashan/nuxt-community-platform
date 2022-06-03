7fb077ceb0fa486b492921afaab75a852a09eef5
mahdikhashan <mahdikhashan@users.noreply.github.com>
2022-06-03 14:23:52 +0000

    deploy: 3eb1fde6987a3139dc1ca1a0a91fece2cf78117a

--- _nuxt/static/1654265286/categories/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654265286",layout:"default",error:a,state:{authentication:{token:b,ip:b},discussion:{discussions:[]},notification:{},auth:{user:a,loggedIn:false,strategy:b}},serverRendered:true,routePath:"\u002Fcategories",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,""));
