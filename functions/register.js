5031b5647e116e2ccd41f419a5bef21a72be9dee
Mahdi Khashan <mkhashan@pyango.ch>
2022-06-03 23:19:34 +0430

    fillout register function

---
+++ functions/register.js
@@ -0,0 +1,9 @@
+exports.handler = async function (event, context) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        token: "my_very_own_private_token",
+      }),
+    };
+  };
+  
